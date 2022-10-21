1d2b18cce9fca43c818611042e073d4403a11e1f
jerry.zhou <jerry.zhou@advantest.com>
2022-10-21 20:32:33 +0800

    Update alis.

--- src/views/home.ts
+++ src/views/home.ts
@@ -1,4 +1,4 @@
-import { userInfo, sum } from "./../utils/tools";
+import { userInfo, sum } from "@/utils/tools";
 
 export default () => {
   console.log(sum(5, 3));
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -56,7 +56,7 @@ module.exports = {
   ],
   resolve: {
     alias: {
-      "@/": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"),
     },
     extensions: [".js", ".ts", ".tsx", ".jsx"],
   },
